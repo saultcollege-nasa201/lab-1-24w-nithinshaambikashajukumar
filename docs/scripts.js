61379057161d3e915ea1dc233909d723459f27e3
nithinshaambikashajukumar <157163251+nithinshaambikashajukumar@users.noreply.github.com>
2024-02-16 19:27:18 +0000

    Created a <div> for characterError and completed the name input by allowing only alphabets

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,17 +1,12 @@
-document.forms.contactForm.addEventListner('submit', (event) => {
+document.forms.contactForm.addEventListener("submit", (event) => {
+  let nameInput = document.getElementById("name");
+  let emailInput = document.getElementById("email");
+  let messageInput = document.getElementById("message");
 
-let nameInput = document.getElementById('name');
-let emailInput = document.getElementById('email');
-let messageInput = document.getElementById('message');
-
-if ( ! /^[A-Za-z]{1,16}$/.test(nameInput)  )
-{
-    document.getElementById("character error").textContent = "NO SPECIAL CHARACTERS ALLOWED!!!"
-event.preventDefault();
-console.log("prevent submit")
-}
-}
-
-
-
-)
+  if (!/^[A-Za-z]{1,16}$/.test(nameInput.value)) {
+    document.getElementById("characterError").textContent =
+      "NO SPECIAL CHARACTERS ALLOWED!!!";
+    event.preventDefault();
+    console.log("prevent submit");
+  }
+});
