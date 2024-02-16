d8a677ea557c68cf8689306acc72e20d603e9ef3
nithinshaambikashajukumar <157163251+nithinshaambikashajukumar@users.noreply.github.com>
2024-02-16 19:34:40 +0000

    edited the error message

--- docs/scripts.js
+++ docs/scripts.js
@@ -10,8 +10,8 @@ document.forms.contactForm.addEventListener("submit", (event) => {
     console.log("prevent submit");
   }
   if (!/^[A-Za-z0-9.,!""]*$/.test(messageInput.value)) {
-    document.getElementById("characterError").textContent =
-      "NO SPECIAL CHARACTERS ALLOWED!!!";
+    document.getElementById("messageError").textContent =
+      "NO SPECIAL CHARACTERS ALLOWED IN THE NAME FIELD!!!";
     event.preventDefault();
     console.log("prevent submit");
   }
