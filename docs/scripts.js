d73dc32139af1f75297bdbbbe1192bfd671803b1
nithinshaambikashajukumar <157163251+nithinshaambikashajukumar@users.noreply.github.com>
2024-02-17 04:08:14 +0000

    added a missing +

--- docs/scripts.js
+++ docs/scripts.js
@@ -3,11 +3,20 @@ document.forms.contactForm.addEventListener("submit", (event) => {
   let emailInput = document.getElementById("email");
   let messageInput = document.getElementById("message");
 
-  if (!/^[A-Za-z]$/.test(nameInput.value)) {
+  if (!/^[A-Za-z]+$/.test(nameInput.value)) {
     document.getElementById("characterError").textContent =
       "NO SPECIAL CHARACTERS ALLOWED!!!";
     event.preventDefault();
     console.log("prevent submit");
+  }
+
+  function validateName(name) {
+    if (name.length <= 5) {
+      return true;
+    } else {
+      console.log("Name is too long");
+      return false;
+    }
   }
 
   if (!/^[A-Za-z0-9.,!]*$/.test(messageInput.value)) {
