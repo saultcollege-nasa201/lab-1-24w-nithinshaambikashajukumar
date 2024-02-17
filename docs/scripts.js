701a9fd0841573618fd071f2749b396aa8643da3
nithinshaambikashajukumar <157163251+nithinshaambikashajukumar@users.noreply.github.com>
2024-02-17 04:05:17 +0000

    removed name length from the current "if" function

--- docs/scripts.js
+++ docs/scripts.js
@@ -3,12 +3,13 @@ document.forms.contactForm.addEventListener("submit", (event) => {
   let emailInput = document.getElementById("email");
   let messageInput = document.getElementById("message");
 
-  if (!/^[A-Za-z]{1,16}$/.test(nameInput.value)) {
+  if (!/^[A-Za-z]$/.test(nameInput.value)) {
     document.getElementById("characterError").textContent =
       "NO SPECIAL CHARACTERS ALLOWED!!!";
     event.preventDefault();
     console.log("prevent submit");
   }
+
   if (!/^[A-Za-z0-9.,!]*$/.test(messageInput.value)) {
     document.getElementById("messageError").textContent =
       "NO SPECIAL CHARACTERS ALLOWED IN THIS FIELD EXCEPT . , ! ";
